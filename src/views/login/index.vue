4e953bbadabdf88ec85bc3a21ef70bf05a9b3f1d
Hello-XY <1661230401@qq.com>
2022-07-30 22:32:38 +0800

    完成login静态页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <div class="login-div">
+      <img src="	http://likede2-admin.itheima.net/img/logo.595745bd.png" alt="" class="login-logo">
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
-      <div class="title-container">
+      <!-- <div class="title-container">
         <h3 class="title">Login Form</h3>
-      </div>
+      </div> -->
 
       <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
+        <span class="svg-container ">
+          <i class="el-icon-mobile-phone"></i>
         </span>
         <el-input
           ref="username"
           v-model="loginForm.username"
-          placeholder="Username"
+          placeholder="请输入账号"
           name="username"
           type="text"
           tabindex="1"
@@ -30,7 +32,7 @@
           ref="password"
           v-model="loginForm.password"
           :type="passwordType"
-          placeholder="Password"
+          placeholder="请输入密码"
           name="password"
           tabindex="2"
           auto-complete="on"
@@ -41,14 +43,28 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-form-item prop="verificationCode">
+        <div class="el-row is-justify-space-between el-row--flex">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input
+            ref="verificationCode"
+            v-model="loginForm.verificationCode"
+            placeholder="请输入验证码"
+            name="verificationCode"
+            type="text"
+            tabindex="1"
+            auto-complete="on"
+          />
+            <img data-v-26affb70="" src="https://likede2-java.itheima.net/api/user-service/user/imageCode/GgK19JFMGdz9qbQ4tqDZ2ACEy1x8zVUZ" class="code-image">
+        </div>
+      </el-form-item>
 
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;height:52px;" @click.native.prevent="handleLogin">登录</el-button>
 
     </el-form>
+    </div>
   </div>
 </template>
 
@@ -60,14 +76,21 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入账号'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('请输入密码'))
+      } else {
+        callback()
+      }
+    }
+    const validateVerificationCode = (rule, value, callback) => {
+      if (value.length < 4) {
+        callback(new Error('请输入验证码'))
       } else {
         callback()
       }
@@ -75,11 +98,13 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: '111111'
+        password: '111111',
+        verificationCode: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        password: [{ required: true, trigger: 'blur', validator: validatePassword }],
+        verificationCode: [{ required: true, trigger: 'blur', validator: validateVerificationCode }]
       },
       loading: false,
       passwordType: 'password',
@@ -130,8 +155,8 @@ export default {
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
 $bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
+$light_gray:#999;
+$cursor: #000;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
@@ -165,7 +190,7 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    background: #fff;
     border-radius: 5px;
     color: #454545;
   }
@@ -182,12 +207,40 @@ $light_gray:#eee;
   width: 100%;
   background-color: $bg;
   overflow: hidden;
-
-  .login-form {
+  .login-div{
+    position: absolute;
+    width: 518px;
+    height: 388px;
+    top: 50%;
+    left: 50%;
+    margin-top: -194px;
+    margin-left: -259px;
+    padding: 76px 35px 0;
+    background: #fff;
+    box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    border-radius: 10px;
+    .login-logo{
+      position: absolute;
+      width: 96px;
+      height: 96px;
+      top: -46px;
+      left: 50%;
+      margin-left: -48px;
+    }
+    .el-form-item{
+      width: 100%;
+      height: 52px;
+      margin-bottom: 24px;
+      background: #fff;
+      border: 1px solid #e2e2e2;
+      border-radius: 4px;
+    }
+}
+      .login-form {
     position: relative;
     width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
+    // padding: 160px 35px 0;
     margin: 0 auto;
     overflow: hidden;
   }
@@ -224,6 +277,17 @@ $light_gray:#eee;
     }
   }
 
+  .el-button--primary{
+    width: 100%;
+    height: 52px;
+    background: linear-gradient(262deg,#2e50e1,#6878f0);
+    opacity: .91;
+    border-radius: 8px;
+    color: #fff;
+    text-shadow: 0 7px 22px #cfcfcf;
+  }
+}
+
   .show-pwd {
     position: absolute;
     right: 10px;
@@ -233,5 +297,4 @@ $light_gray:#eee;
     cursor: pointer;
     user-select: none;
   }
-}
 </style>
